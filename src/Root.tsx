c9bd80a306376d756b79a046a3f010f8d88a760d
littlehorseboy <littlehorseboy@gmail.com>
2019-10-27 21:27:40 +0800

    feat: 年月日切換大致完成 todo: 時分 UI 排版

---
+++ src/Root.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+export default function Root(): JSX.Element {
+  return (
+    <>
+      <CssBaseline />
+    </>
+  );
+}
